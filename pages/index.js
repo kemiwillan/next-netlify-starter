907499c5d83401b92726cb69fcba4b562869a915
kemiwillan <96294303+kemiwillan@users.noreply.github.com>
2022-07-21 15:16:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+    var img = document.createElement("d9a8263e-26ff-4d72-8a0e-3aab2b28917e.png");
+    img.src = "matplotlib-grid-02.png";
+
+    var div = document.getElementById("x");
+    div.appendChild(img);
+    //block.setAttribute("style", "text-align:center");
+  
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
